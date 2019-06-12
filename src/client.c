2a30224dd803b6f1bdfc5f399dd85b19cc1be96c
AnyKeyShik <nikitag594@gmail.com>
2019-06-12 15:08:03 +0300

    Update client

--- src/client.c
+++ src/client.c
@@ -7,70 +7,82 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg);
+void raise_error(const char *msg);
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    char buffer[256];
-    
+    int sockfd, portno;
+    char buffer[1024];
     struct sockaddr_in serv_addr;
     struct hostent *server;
     
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       
-       exit(0);
+    int check; // Var for check functions return
+
+    if(argc < 3) 
+    {
+        raise_error("No port or hostname provided");
     }
     
-    portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (sockfd < 0) {
-        error("ERROR opening socket");
+    if(sockfd < 0) 
+    {
+        raise_error("Opening socket");
+    }
+
+    portno = atoi(argv[2]);
+    if(portno < 1 || portno > 65535)
+    {
+        raise_error("Incorrect port number");
     }
     
     server = gethostbyname(argv[1]);
-    
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        
-        exit(0);
+    if(server == NULL) 
+    {
+        raise_error("No such host");
     }
     
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+    bcopy(server->h_addr, &serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        error("ERROR connecting");
+    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    {
+        raise_error("Can't connection to the destanation host");
     }
     
     printf("Please enter your name: ");
-    
     bzero(buffer, 256);
-    fgets(buffer, 256, stdin);
+    fgets(buffer, 255, stdin);
+    strtok(buffer, "\n");
+    
+    check = write(sockfd, buffer, strlen(buffer));
+    if(check < 0)
+    {
+        raise_error("Writing to socket");
+    }
 
-
-
-    while(1) {
+    while(1) 
+    {
         printf("Please enter the message: ");
         
-        bzero(buffer, 256);
-        fgets(buffer, 255, stdin);
+        bzero(buffer, 1024);
+        fgets(buffer, 1023, stdin);
+        strtok(buffer, "\n");
         
-        n = write(sockfd, buffer, strlen(buffer));
-        
-        if (n < 0) {
-            error("ERROR writing to socket");
+        check = write(sockfd, buffer, strlen(buffer));
+        if (check < 0) 
+        {
+            raise_error("Writing to socket");
         }
         
-        bzero(buffer, 256);
-        n = read(sockfd, buffer,255);
-        
-        if (n < 0) {
-            error("ERROR reading from socket");
+        bzero(buffer, 1024);
+
+        check = read(sockfd, buffer, 1023);
+        if (check < 0) 
+        {
+            raise_error("Reading from socket");
         }
         
         printf("%s\n",buffer);
@@ -81,8 +93,9 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void error(const char *msg) {
-    perror(msg);
+void raise_error(const char *msg) 
+{
+    fprintf(stderr, "\033[0;31m==> ERROR: %s \033[0m\n", msg);
     
     exit(1);
 }
