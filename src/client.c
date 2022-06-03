f6d09cb1975d56ee5123528f3367abe5512b1537
nendensu <efremov.is19@physics.msu.ru>
2022-06-03 23:59:40 +0300

    Added EOF for blank messages

--- src/client.c
+++ src/client.c
@@ -18,26 +18,22 @@ int main(int argc, char *argv[])
     
     int check; // Var for check functions return
 
-    if(argc < 3) 
-    {
+    if(argc < 3){
         raise_error("No port or hostname provided");
     }
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) 
-    {
+    if(sockfd < 0){
         raise_error("Opening socket");
     }
 
     portnum = atoi(argv[2]);
-    if(portnum < 1 || portnum > 65535)
-    {
+    if(portnum < 1 || portnum > 65535){
         raise_error("Incorrect port number");
     }
     
     server = gethostbyname(argv[1]);
-    if(server == NULL) 
-    {
+    if(server == NULL){
         raise_error("No such host");
     }
     
@@ -47,15 +43,14 @@ int main(int argc, char *argv[])
     bcopy(server->h_addr, &serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portnum);
     
-    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-    {
+    if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
         raise_error("Can't connection to the destanation host");
     }
 
     printf("\033[0;32m---> Successfully connect to the %s on port %d\033[0m\n", argv[1], portnum);
     fflush(stdout);
     
-    printf("Please enter your name: ");
+    puts("Please enter your name: ");
     fflush(stdout);
 
     bzero(buffer, 256);
@@ -63,16 +58,14 @@ int main(int argc, char *argv[])
     strtok(buffer, "\n");
     
     check = write(sockfd, buffer, strlen(buffer));
-    if(check < 0)
-    {
+    if(check < 0){
         raise_error("Writing to socket");
     }
     
     bzero(buffer, 1024);
 
     check = read(sockfd, buffer, 1023);
-    if (check < 0) 
-    {
+    if (check < 0){
         raise_error("Reading from socket");
     }
         
@@ -86,19 +79,22 @@ int main(int argc, char *argv[])
         
         bzero(buffer, 1024);
         fgets(buffer, 1023, stdin);
+
+        if (feof(stdin)){
+            break;
+        }
+
         strtok(buffer, "\n");
         
         check = write(sockfd, buffer, strlen(buffer));
-        if (check < 0) 
-        {
+        if (check < 0){
             raise_error("Writing to socket");
         }
         
         bzero(buffer, 1024);
 
         check = read(sockfd, buffer, 1023);
-        if (check < 0) 
-        {
+        if (check < 0){
             raise_error("Reading from socket");
         }
         
